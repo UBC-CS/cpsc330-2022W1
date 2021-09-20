26505bb1434a38b827979dd2018e8dec9235d304
Varada Kolhatkar <kvarada@cs.ubc.ca>
2021-09-19 20:16:45 -0700

    updating

--- lectures/code/plotting_functions.py
+++ lectures/code/plotting_functions.py
@@ -154,3 +154,85 @@ def plot_svc_C(param_grid, X_train, y_train, x_label="longitude", y_label='latit
         ax.set_xlabel(x_label)
         ax.set_ylabel(y_label)
     axes[0].legend(loc=1);    
+    
+
+    
+import mglearn
+
+def make_bracket(s, xy, textxy, width, ax):
+    annotation = ax.annotate(
+        s, xy, textxy, ha="center", va="center", size=20,
+        arrowprops=dict(arrowstyle="-[", fc="w", ec="k",
+                        lw=2,), bbox=dict(boxstyle="square", fc="w"))
+    annotation.arrow_patch.get_arrowstyle().widthB = width
+
+    
+def plot_improper_processing(estimator_name):
+    # Adapted from https://github.com/amueller/mglearn/blob/106cf48ef03710ef1402813997746741aa6467da/mglearn/plot_improper_preprocessing.py#L12
+    fig, axes = plt.subplots(2, 1, figsize=(15, 10))
+
+    for axis in axes:
+        bars = axis.barh([0, 0, 0], [11.9, 2.9, 4.9], left=[0, 12, 15],
+                         color=['white', 'grey', 'grey'], hatch="//",
+                         align='edge', edgecolor='k')
+        bars[2].set_hatch(r"")
+        axis.set_yticks(())
+        axis.set_frame_on(False)
+        axis.set_ylim(-.1, 6)
+        axis.set_xlim(-0.1, 20.1)
+        axis.set_xticks(())
+        axis.tick_params(length=0, labeltop=True, labelbottom=False)
+        axis.text(6, -.3, "training folds",
+                  fontdict={'fontsize': 14}, horizontalalignment="center")
+        axis.text(13.5, -.3, "validation fold",
+                  fontdict={'fontsize': 14}, horizontalalignment="center")
+        axis.text(17.5, -.3, "test set",
+                  fontdict={'fontsize': 14}, horizontalalignment="center")
+
+    make_bracket("scaler fit", (7.5, 1.3), (7.5, 2.), 15, axes[0])
+    make_bracket(estimator_name + " fit", (6, 3), (6, 4), 12, axes[0])
+    make_bracket(estimator_name + "predict", (13.4, 3), (13.4, 4), 2.5, axes[0])
+
+    axes[0].set_title("Cross validation")
+    axes[1].set_title("Test set prediction")
+
+    make_bracket("scaler fit", (7.5, 1.3), (7.5, 2.), 15, axes[1])
+    make_bracket(estimator_name + " fit", (7.5, 3), (7.5, 4), 15, axes[1])
+    make_bracket(estimator_name + " predict", (17.5, 3), (17.5, 4), 4.8, axes[1])    
+    
+    
+def plot_proper_processing(estimator_name):
+    # Adapted from https://github.com/amueller/mglearn/blob/106cf48ef03710ef1402813997746741aa6467da/mglearn/plot_improper_preprocessing.py#L12
+    
+    fig, axes = plt.subplots(2, 1, figsize=(15, 8))
+
+    for axis in axes:
+        bars = axis.barh([0, 0, 0], [11.9, 2.9, 4.9],
+                         left=[0, 12, 15], color=['white', 'grey', 'grey'],
+                         hatch="//", align='edge', edgecolor='k')
+        bars[2].set_hatch(r"")
+        axis.set_yticks(())
+        axis.set_frame_on(False)
+        axis.set_ylim(-.1, 4.5)
+        axis.set_xlim(-0.1, 20.1)
+        axis.set_xticks(())
+        axis.tick_params(length=0, labeltop=True, labelbottom=False)
+        axis.text(6, -.3, "training folds", fontdict={'fontsize': 14},
+                  horizontalalignment="center")
+        axis.text(13.5, -.3, "validation fold", fontdict={'fontsize': 14},
+                  horizontalalignment="center")
+        axis.text(17.5, -.3, "test set", fontdict={'fontsize': 14},
+                  horizontalalignment="center")
+
+    make_bracket("scaler fit", (6, 1.3), (6, 2.), 12, axes[0])
+    make_bracket(estimator_name + " fit", (6, 3), (6, 4), 12, axes[0])
+    make_bracket(estimator_name + " predict", (13.4, 3), (13.4, 4), 2.5, axes[0])
+
+    axes[0].set_title("Cross validation")
+    axes[1].set_title("Test set prediction")
+
+    make_bracket("scaler fit", (7.5, 1.3), (7.5, 2.), 15, axes[1])
+    make_bracket(estimator_name + " fit", (7.5, 3), (7.5, 4), 15, axes[1])
+    make_bracket(estimator_name + " predict", (17.5, 3), (17.5, 4), 4.8, axes[1])
+    fig.subplots_adjust(hspace=.3)
+    
