a0791c1d9111f47ea5620228ad3b47872b38d438
Varada Kolhatkar <kvarada@cs.ubc.ca>
2021-09-25 21:21:42 -0700

    updating

--- lectures/code/utils.py
+++ lectures/code/utils.py
@@ -5,6 +5,7 @@ from PIL import Image
 from torchvision.models import vgg16
 from torchvision import transforms
 import torch
+from sklearn.model_selection import cross_val_score, cross_validate, train_test_split
 
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor, export_graphviz
 
@@ -91,3 +92,32 @@ def cross_validate_std(*args, **kwargs):
     if "train_score" in res:
         res_mean["std_train_score"] = res["train_score"].std()
     return res_mean
+
+def mean_std_cross_val_scores(model, X_train, y_train, **kwargs):
+    """
+    Returns mean and std of cross validation
+
+    Parameters
+    ----------
+    model :
+        scikit-learn model
+    X_train : numpy array or pandas DataFrame
+        X in the training data
+    y_train :
+        y in the training data
+
+    Returns
+    ----------
+        pandas Series with mean scores from cross_validation
+    """
+
+    scores = cross_validate(model, X_train, y_train, **kwargs)
+
+    mean_scores = pd.DataFrame(scores).mean()
+    std_scores = pd.DataFrame(scores).std()
+    out_col = []
+
+    for i in range(len(mean_scores)):
+        out_col.append((f"%0.3f (+/- %0.3f)" % (mean_scores[i], std_scores[i])))
+
+    return pd.Series(data=out_col, index=mean_scores.index)
